168dbf2e2072a1a1744acb789876fd55a3b4a80b
roxyabedi <roxy.abedi@gmail.com>
2023-10-23 22:43:44 -0700

    updates

--- index.js
+++ index.js
@@ -8,48 +8,59 @@ const port = 3000;
 
 //scott was here
 
-const queryParams = {
-  player_ids: 70,
-  seasons: 2022,
-};
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let playerNameInput = "";
 let apiResults = {};
+let points = {};
+
+// app.get("/", async (req, res) => {
+//   res.render("index.ejs", apiResults);
+// });
 
 app.get("/", async (req, res) => {
-  res.render("index.ejs", apiResults);
+  res.render("index.ejs", points);
 });
 
-// app.get("/average", async (req, res) => {
-//   try {
-//     const result = await axios.get(
-//       "https://www.balldontlie.io/api/v1/season_averages?season=2022&player_ids[]=434"
-//     );
-//     res.render("index.ejs", result.data.data[0]);
-//   } catch (error) {
-//     console.log(error.response.data);
-//   }
-// });
+app.post("/", async (req, res) => {
+  try {
+    const result = await axios.get(
+      "https://www.balldontlie.io/api/v1/season_averages?season=2022&player_ids[]=434"
+    );
+    res.render("index.ejs", result.data.data[0]);
+  } catch (error) {
+    console.log(error.response.data);
+  }
+});
 
 app.post("/", async (req, res) => {
   playerNameInput = req.body["playerNameInput"];
   try {
-    const result = await axios.get(
-      `https://www.balldontlie.io/api/v1/players?search=${playerNameInput}`
-    );
-
+    const result = await axios.get(`https://www.balldontlie.io/api/v1/players?search=${playerNameInput}`);
+    
+   
     apiResults = result.data.data[0];
-
-    console.log("scotttest apiResilts", apiResults);
+    console.log("scotttest apiResults", apiResults);
 
     res.redirect("/");
   } catch (error) {
     console.log(((error || {}).response || {}).data);
   }
 });
+
+// app.post("/", async (req, res) => {
+//     try {
+//       //const result = await axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=2022&player_ids[]=${playerID}`);
+//       const result = await axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=2022&player_ids[]=434`);
+//       //playerID = result.data.data.id; 
+//       points = result.data.data[0];
+//       console.log("roxtest", points);
+    
+//     } catch (error) {
+//       console.log(((error || {}).response || {}).data);
+//     }
+// });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
