d69514f8a4651cc964b40b966ba09cd48678891a
scottsak <48599109+scottsak@users.noreply.github.com>
2023-10-23 23:54:01 -0700

    added second call

--- index.js
+++ index.js
@@ -12,37 +12,37 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let playerNameInput = "";
-let apiResults = {};
+let playerInformation = {};
 let points = {};
 
-// app.get("/", async (req, res) => {
-//   res.render("index.ejs", apiResults);
-// });
-
+//gets called when the page is loaded
 app.get("/", async (req, res) => {
-  res.render("index.ejs", points);
+  res.render("index.ejs", playerInformation);
 });
 
+//gets called when the submit button is pressed
 app.post("/", async (req, res) => {
-  try {
-    const result = await axios.get(
-      "https://www.balldontlie.io/api/v1/season_averages?season=2022&player_ids[]=434"
-    );
-    res.render("index.ejs", result.data.data[0]);
-  } catch (error) {
-    console.log(error.response.data);
-  }
-});
-
-app.post("/", async (req, res) => {
+  //gets the value of the submit button
   playerNameInput = req.body["playerNameInput"];
   try {
-    const result = await axios.get(`https://www.balldontlie.io/api/v1/players?search=${playerNameInput}`);
-    
-   
-    apiResults = result.data.data[0];
-    console.log("scotttest apiResults", apiResults);
+    //makes call to the api for player information
+    const apiPlayerInfo = await axios.get(
+      `https://www.balldontlie.io/api/v1/players?search=${playerNameInput}`
+    );
 
+    //sets the object to be passed to the ejs
+    playerInformation = apiPlayerInfo.data.data[0];
+
+    const playerId = playerInformation.id; //player id
+    console.log("scotttest playerId", playerId);
+
+    //makes call to the api for player stats
+    const playerStats = await axios.get(
+      `https://www.balldontlie.io/api/v1/season_averages?season=2022&player_ids[]=${playerId}`
+    );
+    console.log("scotttest playerStats", playerStats.data.data[0]);
+
+    //reloads the page
     res.redirect("/");
   } catch (error) {
     console.log(((error || {}).response || {}).data);
@@ -53,10 +53,10 @@ app.post("/", async (req, res) => {
 //     try {
 //       //const result = await axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=2022&player_ids[]=${playerID}`);
 //       const result = await axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=2022&player_ids[]=434`);
-//       //playerID = result.data.data.id; 
+//       //playerID = result.data.data.id;
 //       points = result.data.data[0];
 //       console.log("roxtest", points);
-    
+
 //     } catch (error) {
 //       console.log(((error || {}).response || {}).data);
 //     }
