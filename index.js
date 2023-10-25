5f8af6bb1a36aebfaa0d83f7d63b61bd29d3f18a
scottsak <48599109+scottsak@users.noreply.github.com>
2023-10-24 22:10:58 -0700

    Adding second player information and adding second submit

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import axios from "axios";
 import ejs from "ejs";
 import bodyParser from "body-parser";
 
+// array = []
+// object = {}
+//how to do ______ with object/array/string in js
+
 const app = express();
 const port = 3000;
 
@@ -11,19 +15,19 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let playerNameInput = "";
-let playerInformation = {};
-let seasonStats = {};
+let player0 = {};
+let player1 = {};
+let player2 = {};
 
 //gets called when the page is loaded
 app.get("/", async (req, res) => {
-  res.render("index.ejs", playerInformation);
+  res.render("index.ejs", [player0, player1, player2]);
 });
 
 //gets called when the submit button is pressed
 app.post("/", async (req, res) => {
   //gets the value of the submit button
-  playerNameInput = req.body["playerNameInput"];
+  const playerNameInput = req.body["playerNameInput"];
   try {
     //makes call to the api for player information
     const apiPlayerInfo = await axios.get(
@@ -31,21 +35,19 @@ app.post("/", async (req, res) => {
     );
 
     //sets the object to be passed to the ejs
-    playerInformation = apiPlayerInfo.data.data[0];
+    const playerInformation = apiPlayerInfo.data.data[0];
 
     const playerId = playerInformation.id; //player id
-    console.log("scotttest playerId", playerId);
 
     //makes call to the api for player stats
     const playerStats = await axios.get(
       `https://www.balldontlie.io/api/v1/season_averages?season=2022&player_ids[]=${playerId}`
     );
 
-    seasonStats = playerStats.data.data[0];
+    const seasonStats = playerStats.data.data[0];
 
-    const points = seasonStats.pts;
-    console.log("roxtest points", points);
-    // console.log("scotttest playerStats", playerStats.data.data[0]);
+    //combines player information
+    player0 = Object.assign(playerInformation, seasonStats);
 
     //reloads the page
     res.redirect("/");
@@ -54,6 +56,36 @@ app.post("/", async (req, res) => {
   }
 });
 
+app.post("/player1", async (req, res) => {
+  //gets the value of the submit button
+  const playerNameInput = req.body["playerCompOne"];
+  try {
+    //makes call to the api for player information
+    const apiPlayerInfo = await axios.get(
+      `https://www.balldontlie.io/api/v1/players?search=${playerNameInput}`
+    );
+
+    //sets the object to be passed to the ejs
+    const playerInformation = apiPlayerInfo.data.data[0];
+
+    const playerId = playerInformation.id; //player id
+
+    //makes call to the api for player stats
+    const playerStats = await axios.get(
+      `https://www.balldontlie.io/api/v1/season_averages?season=2022&player_ids[]=${playerId}`
+    );
+
+    const seasonStats = playerStats.data.data[0];
+
+    //combines player information
+    player1 = Object.assign(playerInformation, seasonStats);
+
+    //reloads the page
+    res.redirect("/");
+  } catch (error) {
+    console.log(((error || {}).response || {}).data);
+  }
+});
 // app.post("/", async (req, res) => {
 //     try {
 //       //const result = await axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=2022&player_ids[]=${playerID}`);
