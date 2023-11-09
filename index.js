2bda8190974d3c1d71a460341940d0262b394d37
roxyabedi <roxy.abedi@gmail.com>
2023-11-08 22:40:40 -0800

    render

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import bodyParser from "body-parser";
 const app = express();
 const port = process.env.PORT || 3030;
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
