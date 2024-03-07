88c5cd9ed55d8832c5ab32feec488ca950246edf
roxyabedi <roxy.abedi@gmail.com>
2024-03-06 23:26:27 -0800

    update code to reflect updated api documentation

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3030;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// const process.env.API_KEY = '28186af8-31c8-4c0b-a7fb-a88172da42b2';
+process.env.API_KEY = '28186af8-31c8-4c0b-a7fb-a88172da42b2';
 
 let player0 = {};
 let player1 = {};
@@ -98,16 +98,35 @@ app.post('/', async (req, res) => {
 app.post('/player1', async (req, res) => {
   //gets the value of the submit button
   const playerNameInput = req.body['playerCompOne'];
+  const [firstName, lastName] = playerNameInput.split(' '); // ['Jayson', 'Tatum']
   try {
     //makes call to the api for player information
-    const apiPlayerInfo = await axiosGetWithAuthorization(
-      `http://api.balldontlie.io/v1/players?search=${playerNameInput}`
+    const firstNamePlayers = await axiosGetWithAuthorization(
+      `http://api.balldontlie.io/v1/players?search=${firstName}`
     );
 
-    //sets the object to be passed to the ejs
-    const playerInformation = apiPlayerInfo.data[0];
+    console.log('scotttest firstNamePlayers', firstNamePlayers);
+
+    const firstNameSet = new Set(
+      firstNamePlayers.data.map(
+        (player) => `${player.first_name} ${player.last_name}`
+      )
+    );
+
+    const lastNamePlayers = await axiosGetWithAuthorization(
+      `http://api.balldontlie.io/v1/players?search=${lastName}`
+    );
+
+    console.log('scotttest lastNamePlayers', lastNamePlayers);
+
+    const playerInformation = lastNamePlayers.data.length
+      ? lastNamePlayers.data.find((player) =>
+          firstNameSet.has(`${player.first_name} ${player.last_name}`)
+        )
+      : firstNamePlayers.data[0];
 
     const playerId = playerInformation.id; //player id
+    console.log('roxytest', playerId);
 
     //makes call to the api for player stats
     const playerStats = await axiosGetWithAuthorization(
@@ -129,16 +148,35 @@ app.post('/player1', async (req, res) => {
 app.post('/player2', async (req, res) => {
   //gets the value of the submit button
   const playerNameInput = req.body['playerCompTwo'];
+  const [firstName, lastName] = playerNameInput.split(' '); // ['Jayson', 'Tatum']
   try {
-    //makes call to the api for player information
-    const apiPlayerInfo = await axiosGetWithAuthorization(
-      `http://api.balldontlie.io/v1/players?search=${playerNameInput}`
-    );
-
-    //sets the object to be passed to the ejs
-    const playerInformation = apiPlayerInfo.data[0];
-
-    const playerId = playerInformation.id; //player id
+      //makes call to the api for player information
+      const firstNamePlayers = await axiosGetWithAuthorization(
+        `http://api.balldontlie.io/v1/players?search=${firstName}`
+      );
+  
+      console.log('scotttest firstNamePlayers', firstNamePlayers);
+  
+      const firstNameSet = new Set(
+        firstNamePlayers.data.map(
+          (player) => `${player.first_name} ${player.last_name}`
+        )
+      );
+  
+      const lastNamePlayers = await axiosGetWithAuthorization(
+        `http://api.balldontlie.io/v1/players?search=${lastName}`
+      );
+  
+      console.log('scotttest lastNamePlayers', lastNamePlayers);
+  
+      const playerInformation = lastNamePlayers.data.length
+        ? lastNamePlayers.data.find((player) =>
+            firstNameSet.has(`${player.first_name} ${player.last_name}`)
+          )
+        : firstNamePlayers.data[0];
+  
+      const playerId = playerInformation.id; //player id
+      console.log('roxytest', playerId);
 
     //makes call to the api for player stats
     const playerStats = await axiosGetWithAuthorization(
